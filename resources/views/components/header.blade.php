b0c02b47362627018a1be75f1dc6c105240b58f5
ibnu-zaky <ibnuzaky667@gmail.com>
2024-10-10 15:05:05 +0700

    iben migrate new

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,7 +4,7 @@
     ================================================== -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Webpass</title>
+    <title>Tyndale</title>
 
     <script>
         document.documentElement.classList.remove('no-js');
@@ -13,11 +13,14 @@
 
     <!-- CSS
     ================================================== -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/vendor.css">
     <link rel="stylesheet" href="assets/css/styles.css">
 
     <!-- favicons
     ================================================== -->
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+    <link rel="manifest" href="site.webmanifest">
 
 </head>
--- resources/views/components/script.blade.php
+++ resources/views/components/script.blade.php
@@ -1,3 +1,2 @@
 <script src="assets/js/plugins.js"></script>
 <script src="assets/js/main.js"></script>
-<script src="{{ mix('js/app.js') }}"></script>
